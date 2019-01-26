56f64be57a70aac89f138068856289b477844340
Fabio Brigidi <fabio.brigidi@gmail.com>
2019-01-26 15:36:48 +0100

    Implemented Camera manager

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace GGJ19
+{
+    public class CameraManager : MonoSingleton<CameraManager>
+    {
+        public CinemachineVirtualCamera vCamera;
+
+        public float zoomInSize = 4.5f;
+        public float zoomOutSize = 10f;
+
+        public void StartFollowing(Transform target)
+        {
+            vCamera.m_Follow = target;
+        }
+
+        public void ZoomIn()
+        {
+            StartCoroutine(ZoomInCO());
+        }
+
+        public IEnumerator ZoomInCO()
+        {
+            float zoom = vCamera.m_Lens.OrthographicSize;
+
+            while (vCamera.m_Lens.OrthographicSize > zoomInSize)
+            {
+                vCamera.m_Lens.OrthographicSize = Mathf.Lerp(zoomOutSize, zoomInSize, zoom);
+                zoom += 0.5f * Time.deltaTime;
+                yield return null;
+            }
+
+            vCamera.m_Lens.OrthographicSize = zoomInSize;     
+        }
+
+        public void ZoomOut()
+        {
+            StartCoroutine(ZoomOutCO());
+        }
+
+        public IEnumerator ZoomOutCO()
+        {
+            float zoom = vCamera.m_Lens.OrthographicSize;
+
+            while (vCamera.m_Lens.OrthographicSize > zoomInSize)
+            {
+                vCamera.m_Lens.OrthographicSize = Mathf.Lerp(zoomInSize, zoomOutSize, zoom);
+                zoom += 0.5f * Time.deltaTime;
+                yield return null;
+            }
+
+            vCamera.m_Lens.OrthographicSize = zoomOutSize;
+        }
+    }
+
+}
