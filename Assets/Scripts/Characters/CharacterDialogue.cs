4c25b270a4ca46d31346f1b9083641a99c3ac2e0
FabioBrigidi <fabio.brigidi@gmail.com>
2019-01-26 09:22:40 +0100

    Created dialogue scriptable object

---
+++ Assets/Scripts/Characters/CharacterDialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ19 {
+
+    public enum Emotion { SAD, HAPPY, ANGRY, NORMAL }
+
+    [CreateAssetMenu]
+    public class CharacterDialogue : ScriptableObject
+    {
+        [TextArea]
+        public string text;
+        public Emotion emotion;
+    }
+
+}
