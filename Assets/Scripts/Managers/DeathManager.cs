c9c32988b68cebf022b850007c1c5eb60032770d
FabioBrigidi <fabio.brigidi@gmail.com>
2019-01-27 09:19:58 +0100

    Added death manager

---
+++ Assets/Scripts/Managers/DeathManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ19
+{
+    [SerializeField]
+    public struct DeathEntry
+    {
+        string character;
+        string item;
+        [TextArea]
+        string text;
+    }
+
+    public class DeathManager : MonoBehaviour
+    {
+        public DeathEntry[] entries;
+
+
+
+
+    }
+
+}
