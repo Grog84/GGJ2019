b7199e43d8455d228f6b254ccc3f1615fd37e4b0
FabioBrigidi <fabio.brigidi@gmail.com>
2019-01-27 11:15:27 +0100

    Loaded all audio and continued implemnting main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ19
+{
+
+    public enum MenuBtn { PLAY, CREDITS }
+
+    public class MainMenu : MonoBehaviour
+    {
+        public Button playBtn;
+        public Button creditsBtn;
+
+        public Image selector;
+
+        MenuBtn menuBtn;
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Vertical"))
+            {
+
+            }
+        }
+    }
+
+}
+
