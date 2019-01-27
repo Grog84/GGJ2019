9fae670119733e6d47993d5be0cd487bf4e08bdf
FabioBrigidi <fabio.brigidi@gmail.com>
2019-01-27 01:17:30 +0100

    modified character item

--- Assets/Scripts/Characters/CharacterItem.cs
+++ Assets/Scripts/Characters/CharacterItem.cs
@@ -11,6 +11,7 @@ namespace GGJ19
     [CreateAssetMenu]
     public class CharacterItem: ScriptableObject
     {
+        public string itemName;
         public Sprite sprite;
         public bool isSpecial;
         public Style style;
